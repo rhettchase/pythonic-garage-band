209c38d4a38733e47fd4982dfa200a5f5fe0936c
RHETT CHASE <rhettjchase@gmail.com>
2024-01-11 17:57:27 -0800

    updates Musician class to include get_instrument method

--- band.py
+++ band.py
@@ -1,54 +1,44 @@
 class Musician:
     def __init__(self, name="unknown"):
         self.name = name
+        
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.get_instrument()}"
+    
+    def __repr__(self):
+        return f"Musician instance. Name = {self.name}"
+        
 
 class Guitarist(Musician):
-    def __init__(self, name="unknown"):
-        self.name = name
-    
-    # def play_instrument(self):
-    #     return "Playing the guitar"
 
     def get_instrument(self):
         return "guitar"
 
     def play_solo(self):
         return "face melting guitar solo" 
-        
-    def __str__(self):
-        return f"My name is {self.name} and I play guitar"
     
     def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
 
 class Bassist(Musician):
-    def __init__(self, name="unknown"):
-        self.name = name
-        
+
     def get_instrument(self):
         return "bass"
     
     def play_solo(self):
         return "bom bom buh bom"
-        
-    def __str__(self):
-        return f"My name is {self.name} and I play bass"
+    
     
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
 
 class Drummer(Musician):
-    def __init__(self, name="unknown"):
-        self.name = name
         
     def get_instrument(self):
         return "drums"
     
     def play_solo(self):
         return "rattle boom crash"
-        
-    def __str__(self):
-        return f"My name is {self.name} and I play drums"
     
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
@@ -76,7 +66,8 @@ class Band:
         return cls.instances # returns the list of Band instances
     
     def __str__(self):
-        return ' '.join([str(member) for member in self.members])
+        member_strings = [str(member) for member in self.members] # list comprehension: iterates in place; get str and pass into arr
+        return ' '.join(member_strings)
     
     def __repr__(self):
         return f"Band instance. Name = {self.name}"
